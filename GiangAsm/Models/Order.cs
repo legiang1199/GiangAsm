0c85cd564b99d6323871e0dee722c589eda63916
Giang Nam <Giang Nam@DESKTOP-TH5VOH8>
2022-05-07 13:59:32 +0700

    Add project files.

---
+++ GiangAsm/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using GiangAsm.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GiangAsm.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } = null!;
+
+        public AppUser User { get; set; } = null!;
+
+        public DateTime OrderDate { get; set; }
+
+        public double Total { get; set; }
+
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+    }
+}
