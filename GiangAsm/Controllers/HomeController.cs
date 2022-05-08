a66600ab0dc0f1387e5967a8249b0aa6c0cea689
huutung3112001 <75929238+huutung3112001@users.noreply.github.com>
2022-05-08 16:37:16 +0700

    Render list of book

--- GiangAsm/Controllers/HomeController.cs
+++ GiangAsm/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace GiangAsm.Controllers
@@ -13,21 +14,38 @@ namespace GiangAsm.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IEmailSender _emailSender;
         private readonly UserManager<AppUser> _userManager;
+        private UserContext _context;
 
+        private readonly int maxofpage = 10;
 
-        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender, UserManager<AppUser> userManager)
+        private readonly int rowsonepage = 4;
+        public HomeController(ILogger<HomeController> logger, IEmailSender emailSender, UserManager<AppUser> userManager, UserContext context)
         {
             _logger = logger;
             _emailSender = emailSender;
             _userManager = userManager;
 
+            _context = context;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int id = 0)
         {
-            return View();
+            
+            var books = from s in _context.Book
+                        select s;
+           
+            int numOfFilteredBook = books.Count();
+            ViewBag.NumberOfPages = (int)Math.Ceiling((double)numOfFilteredBook / rowsonepage);
+            ViewBag.CurrentPage = id;
+            List<Book> booklist = await books.Skip(id * rowsonepage)
+                .Take(rowsonepage).ToListAsync();
+            if (id > 0)
+            {
+                ViewBag.idpagprev = id - 1;
+            }
+            ViewBag.idpagenext = id + 1;
+            ViewBag.currentPage = id;
+            return View(booklist);
         }
-
         public async Task<IActionResult> Privacy()
         {
             await _emailSender.SendEmailAsync("legiang1199@gmail.com", "test send mail", "just test");
